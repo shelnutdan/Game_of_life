fc21f6d9f1ffe5643657912d1b9044a65d971988
shelnutdan <shelnutdaniel@gmail.com>
2019-04-13 12:13:56 -0700

    Second commit: added css styling, created a 35 x 35 game gird. Created game logic for active and inactive states when the users clicks the start button. The user can now set the number of generations to iterate through.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Game of life
+This is a simple recreation of John Conway's Game of Life. The user can set the number of generations that they want to iterate through. The user can click on the box to make the box active, this will set the box as "alive". Once the user has the desired configuration on the game board they can click on the start button to see how their configuration will end up after their desired amount of generations.
+
+## Rules
+
+Each of the square boxed has one of two possible states, alive or dead, (or populated and unpopulated, respectively). Every cell interacts with its eight neighbors, which are the boxes that are horizontally, vertically, or diagonally adjacent. At each step in time, the following transitions occur:
+
+  1. Any live boxes with fewer than two live neighbors dies, as if by underpopulation.
+  2. Any live boxes with two or three live neighbors lives on to the next generation.
+  3. Any live box with more than three live neighbors dies, as if by overpopulation.
+  4. Any dead box with exactly three live neighbors becomes a live box, as if by reproduction.
+
+## Live Demo
+
+[Live Demo](http://selective-island.surge.sh/)
+
+## Clone this repo
+
+`$git clone https://github.com/shelnutdan/Game_of_life.git`
+
+## More information on the Game of life
+Check this [interview](https://www.youtube.com/watch?v=E8kUJL04ELA&t=215s) with John Conway himself by NumberPhile
+
+#Future Developments
+
+Allow for slower iteration throughout he generations so that users get a better idea on how their configurations can change over time.
--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
+
+
 let counter=0;
 
 var $container = $("<div></div>").css("float","left");
 
-for(var i = 0; i < 10; i++) {
-    for (var j = 0; j < 10; j++){
+for(var i = 0; i < 35; i++) {
+    for (var j = 0; j < 35; j++){
         $(`<div>${0}</div>`).addClass("box").appendTo($container);
 
     }
     $("<div></div>").css("clear", "both").appendTo($container);
 }
 
-$container.appendTo($("body"));
+$container.appendTo($("#game_board"));
 let box=document.getElementsByClassName("box");
 let array=jQuery.makeArray(box);
 console.log(array)
@@ -21,134 +23,134 @@ $(".box").on("click",function(){
   console.log(array.indexOf(this))
 })
 $("#start").on('click',function(){
+  let generations=$('#generations').val();
+  console.log(generations)
   counter++;
   console.log(counter);
+  while (generations>0){
   console.log("The game of life has started");
+  array.forEach( element =>{
+    element.innerHTML=0});
 
-  console.log($(array[13]).hasClass('active') )
   for(let i=0;i<array.length;i++){
     if(i==0){
       if($(array[i]).hasClass('active')){
         array[1].innerHTML=parseInt(array[1].innerHTML)+1;
-        array[10].innerHTML=parseInt(array[10].innerHTML)+1;
-        array[11].innerHTML=parseInt(array[11].innerHTML)+1;
-        console.log(array[1])
-        console.log(array[10])
-        console.log(array[11])
-      }
-    }
-    if(i==9){
-      if($(array[i]).hasClass('active')){
-        array[8].innerHTML=parseInt(array[8].innerHTML)+1;
-        array[18].innerHTML=parseInt(array[18].innerHTML)+1;
-        array[19].innerHTML=parseInt(array[19].innerHTML)+1;
-        console.log(array[8])
-        console.log(array[18])
-        console.log(array[19])
-      }
-    }if(i==90){
-      if($(array[i]).hasClass('active')){
-        array[80].innerHTML=parseInt(array[80].innerHTML)+1;
-        array[81].innerHTML=parseInt(array[81].innerHTML)+1;
-        array[91].innerHTML=parseInt(array[91].innerHTML)+1;
-        console.log(array[80])
-        console.log(array[81])
-        console.log(array[91])
-      }
-    }
-    if(i==99){
-      if($(array[i]).hasClass('active')){
-        array[88].innerHTML=parseInt(array[88].innerHTML)+1;
-        array[89].innerHTML=parseInt(array[89].innerHTML)+1;
-        array[98].innerHTML=parseInt(array[98].innerHTML)+1;
-        console.log(array[88])
-        console.log(array[89])
-        console.log(array[98])
-      }
-    }
-    if(0<i && i<9){
-      if($(array[i]).hasClass('active')){
-        array[i-1].innerHTML=parseInt(array[i-1].innerHTML)+1;
-        array[i+1].innerHTML=parseInt(array[i+1].innerHTML)+1;
-        array[i+9].innerHTML=parseInt(array[i+9].innerHTML)+1;
-        array[i+10].innerHTML=parseInt(array[i+10].innerHTML)+1;
-        array[i+11].innerHTML=parseInt(array[i+11].innerHTML)+1;
-        console.log(array[i-1])
-        console.log(array[i+1])
-        console.log(array[i+9])
-        console.log(array[i+10])
-        console.log(array[i+11])
+        array[35].innerHTML=parseInt(array[35].innerHTML)+1;
+        array[36].innerHTML=parseInt(array[36].innerHTML)+1;
 
       }
     }
-    if(90<i && i<99){
+    if(i==34){
+      if($(array[i]).hasClass('active')){
+        array[33].innerHTML=parseInt(array[33].innerHTML)+1;
+        array[68].innerHTML=parseInt(array[68].innerHTML)+1;
+        array[69].innerHTML=parseInt(array[69].innerHTML)+1;
+
+      }
+    }if(i==1190){
+      if($(array[i]).hasClass('active')){
+        array[1155].innerHTML=parseInt(array[1155].innerHTML)+1;
+        array[1156].innerHTML=parseInt(array[1156].innerHTML)+1;
+        array[1191].innerHTML=parseInt(array[1191].innerHTML)+1;
+
+      }
+    }
+    if(i==1224){
+      if($(array[i]).hasClass('active')){
+        array[1188].innerHTML=parseInt(array[1188].innerHTML)+1;
+        array[1189].innerHTML=parseInt(array[1189].innerHTML)+1;
+        array[1123].innerHTML=parseInt(array[1123].innerHTML)+1;
+
+      }
+    }
+    if(0<i && i<34){
       if($(array[i]).hasClass('active')){
         array[i-1].innerHTML=parseInt(array[i-1].innerHTML)+1;
         array[i+1].innerHTML=parseInt(array[i+1].innerHTML)+1;
-        array[i-9].innerHTML=parseInt(array[i-9].innerHTML)+1;
-        array[i-10].innerHTML=parseInt(array[i-10].innerHTML)+1;
-        array[i-11].innerHTML=parseInt(array[i-11].innerHTML)+1;
-        console.log(array[i-1])
-        console.log(array[i+1])
-        console.log(array[i-9])
-        console.log(array[i-10])
-        console.log(array[i-11])
+        array[i+34].innerHTML=parseInt(array[i+34].innerHTML)+1;
+        array[i+35].innerHTML=parseInt(array[i+35].innerHTML)+1;
+        array[i+36].innerHTML=parseInt(array[i+36].innerHTML)+1;
+
+
       }
     }
-    if(i==10 || i==20 || i==30 || i==40 || i==50 || i==60 || i==70 || i==80){
+    if(1190<i && i<1224){
       if($(array[i]).hasClass('active')){
-        array[i-10].innerHTML=parseInt(array[i-10].innerHTML)+1;
-        array[i+10].innerHTML=parseInt(array[i+10].innerHTML)+1;
-        array[i+11].innerHTML=parseInt(array[i+11].innerHTML)+1;
-        array[i-9].innerHTML=parseInt(array[i-9].innerHTML)+1;
-        array[i+1].innerHTML=parseInt(array[i+11].innerHTML)+1;
-        console.log(array[i-10])
-        console.log(array[i+10])
-        console.log(array[i-9])
-        console.log(array[i-10])
-        console.log(array[i+1])
-      }
-    }
-    if(i==19 || i==29 || i==39 || i==49 || i==59 || i==69 || i==79 || i==89){
-      if($(array[i]).hasClass('active')){
-        array[i-10].innerHTML=parseInt(array[i-10].innerHTML)+1;
-        array[i+10].innerHTML=parseInt(array[i+10].innerHTML)+1;
-        array[i-11].innerHTML=parseInt(array[i-11].innerHTML)+1;
-        array[i+9].innerHTML=parseInt(array[i+9].innerHTML)+1;
         array[i-1].innerHTML=parseInt(array[i-1].innerHTML)+1;
-        console.log(array[i-10])
-        console.log(array[i+10])
-        console.log(array[i+9])
-        console.log(array[i-11])
-        console.log(array[i-1])
+        array[i+1].innerHTML=parseInt(array[i+1].innerHTML)+1;
+        array[i-34].innerHTML=parseInt(array[i-34].innerHTML)+1;
+        array[i-35].innerHTML=parseInt(array[i-35].innerHTML)+1;
+        array[i-36].innerHTML=parseInt(array[i-36].innerHTML)+1;
+
+      }
+    }
+    if(i==35 || i==70 || i==105 || i==140 || i==175 || i==210 || i==245 || i==280 ||i==315 || i==350 || i==385 || i==420 || i==455 || i==490 || i==525 || i==560 ||i==595 || i==630 || i==665 || i==700 || i==735 || i==770 || i==805 || i==840||i==875 || i==910 || i==945 || i==980 || i==1015 || i==1050 || i==1085 || i==1120 ||i==1155){
+      if($(array[i]).hasClass('active')){
+        array[i-35].innerHTML=parseInt(array[i-35].innerHTML)+1;
+        array[i+35].innerHTML=parseInt(array[i+35].innerHTML)+1;
+        array[i+36].innerHTML=parseInt(array[i+36].innerHTML)+1;
+        array[i-34].innerHTML=parseInt(array[i-34].innerHTML)+1;
+        array[i+1].innerHTML=parseInt(array[i+1].innerHTML)+1;
+
+      }
+    }
+    if(i==69 || i==104 || i==139 || i==174 || i==209 || i==244 || i==279 || i==314 ||i==349 || i==384 || i==419 || i==454 || i==489 || i==524 || i==559 || i==594||i==629 || i==664 || i==699 || i==734 || i==769 || i==804 || i==839 || i==874||i==909 || i==944 || i==979 || i==1014 || i==1049 || i==1084 || i==1119 || i==1154 ||i==1189){
+      if($(array[i]).hasClass('active')){
+        array[i-35].innerHTML=parseInt(array[i-35].innerHTML)+1;
+        array[i+35].innerHTML=parseInt(array[i+35].innerHTML)+1;
+        array[i-36].innerHTML=parseInt(array[i-36].innerHTML)+1;
+        array[i+34].innerHTML=parseInt(array[i+34].innerHTML)+1;
+        array[i-1].innerHTML=parseInt(array[i-1].innerHTML)+1;
+
       }
     }
     else{
       if($(array[i]).hasClass('active')){
-      array[i-10].innerHTML=parseInt(array[i-10].innerHTML)+1;
-      array[i+10].innerHTML=parseInt(array[i+10].innerHTML)+1;
-      array[i+11].innerHTML=parseInt(array[i+11].innerHTML)+1;
-      array[i-11].innerHTML=parseInt(array[i-11].innerHTML)+1;
-      array[i+9].innerHTML=parseInt(array[i+9].innerHTML)+1;
-      array[i-9].innerHTML=parseInt(array[i-9].innerHTML)+1;
+        if( (0<i && i<34) ||(1190<i && i<1224) || i==35 || i==70 || i==105 || i==140 || i==175 || i==210 || i==245 || i==280 ||i==315 || i==350 || i==385 || i==420 || i==455 || i==490 || i==525 || i==560 ||i==595 || i==630 || i==665 || i==700 || i==735 || i==770 || i==805 || i==840||i==875 || i==910 || i==945 || i==980 || i==1015 || i==1050 || i==1085 || i==1120 ||i==1155 || i==69 || i==104 || i==139 || i==174 || i==209 || i==244 || i==279 || i==314 ||i==349 || i==384 || i==419 || i==454 || i==489 || i==524 || i==559 || i==594||i==629 || i==664 || i==699 || i==734 || i==769 || i==804 || i==839 || i==874||i==909 || i==944 || i==979 || i==1014 || i==1049 || i==1084 || i==1119 || i==1154 ||i==1189){
+          return null;
+        }
+      array[i-35].innerHTML=parseInt(array[i-35].innerHTML)+1;
+      array[i+35].innerHTML=parseInt(array[i+35].innerHTML)+1;
+      array[i+36].innerHTML=parseInt(array[i+36].innerHTML)+1;
+      array[i-36].innerHTML=parseInt(array[i-36].innerHTML)+1;
+      array[i+34].innerHTML=parseInt(array[i+34].innerHTML)+1;
+      array[i-34].innerHTML=parseInt(array[i-34].innerHTML)+1;
       array[i+1].innerHTML=parseInt(array[i+1].innerHTML)+1;
       array[i-1].innerHTML=parseInt(array[i-1].innerHTML)+1;
 
-      console.log(array[i-10])
-      console.log(array[i+10])
-      console.log(array[i+9])
-      console.log(array[i-9])
-      console.log(array[i-11])
-      console.log(array[i+11])
-      console.log(array[i+1])
-      console.log(array[i-1])
+
 
     }
   }
-  }
 
+
+  }
+  array.forEach( element =>{
+    if($(element).text()==3 && !$(element).hasClass('active')){
+      console.log(element)
+      $(element).addClass('active');
+
+    }
+    if( $(element).text()>3 && $(element).hasClass('active')){
+      $(element).removeClass('active')
+      $(element).innerHTML=0;
+    }
+    if($(element).text()<2 && $(element).hasClass('active')){
+      $(element).removeClass('active')
+
+    }
+
+
+  })
+
+   generations--;
+
+   console.log(generations)
+ }
 
 
 })
+
 
 })
